aed4c05db439feab96e34eaacef7782a10d09c67
Hein Zaw Htet <maungheinzawhtet@gmail.com>
2016-04-23 16:16:06 +0630

    first commit

---
+++ index.php
@@ -0,0 +1,485 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+  <script type="text/javascript" src="include/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+	<script type="text/javascript" src="include/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="include/rs-plugin/css/settings.css" media="screen" />
+
+	<!-- Document Title
+	============================================= -->
+	<title>Blank Page | Canvas</title>
+
+</head>
+
+<body class="stretched">
+
+  <header id="header" class="full-header">
+
+    <div id="header-wrap">
+
+      <div class="container clearfix">
+
+        <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+        <!-- Logo
+        ============================================= -->
+        <div id="logo">
+          <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
+          <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+        </div><!-- #logo end -->
+
+        <!-- Primary Navigation
+        ============================================= -->
+        <nav id="primary-menu">
+
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index.html">Services</a></li>
+            <li><a href="index.html">Machines</a></li>
+            <li><a href="index.html">Products</a></li>
+            <li><a href="index.html">About Us</a></li>
+            <li><a href="index.html">Contact Us</a></li>
+          </ul>
+
+          <!-- Top Search
+          ============================================= -->
+          <div id="top-search">
+            <a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+            <form action="search.html" method="get">
+              <input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+            </form>
+          </div><!-- #top-search end -->
+
+        </nav><!-- #primary-menu end -->
+
+      </div>
+
+    </div>
+
+  </header><!-- #header end -->
+
+  <section id="slider" class="slider-parallax revslider-wrap clearfix">
+
+    <!--
+    #################################
+      - THEMEPUNCH BANNER -
+    #################################
+    -->
+    <div class="tp-banner-container">
+      <div class="tp-banner" >
+        <ul>
+      <li class="dark" data-transition="zoomout" data-slotamount="1" data-masterspeed="1500"data-thumb="images/slider/rev/bg2-thumb.jpg"  data-saveperformance="off"  data-title="Fixed-Size Video">
+        <!-- MAIN IMAGE -->
+        <img src="images/slider/rev/bg2.jpg">
+        <!-- LAYERS -->
+
+        <div class="tp-caption customin"
+        data-x="0"
+        data-y="0"
+        data-customin="x:0;y:0;z:0;rotationZ:0;scaleX:0.6;scaleY:0.6;skewX:0;skewY:0;opacity:0;transformPerspective:0;transformOrigin:50% 50%;"
+        data-speed="400"
+        data-start="1000"
+        data-easing="easeOutQuad"
+        data-splitin="none"
+        data-splitout="none"
+        data-elementdelay="0.01"
+        data-endelementdelay="0.1"
+        data-endspeed="1000"
+        data-endeasing="Power4.easeIn"><img src="images/slider/rev/stock.png"></div>
+
+        <div class="tp-caption customin ltl tp-resizeme revo-slider-caps-text uppercase"
+        data-x="625"
+        data-y="165"
+        data-customin="x:140;y:0;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+        data-speed="800"
+        data-start="1000"
+        data-easing="easeOutQuad"
+        data-splitin="none"
+        data-splitout="none"
+        data-elementdelay="0.01"
+        data-endelementdelay="0.1"
+        data-endspeed="1000"
+        data-endeasing="Power4.easeIn">Need to show Videos?</div>
+
+        <div class="tp-caption customin ltl tp-resizeme revo-slider-emphasis-text nopadding noborder"
+        data-x="622"
+        data-y="180"
+        data-customin="x:140;y:0;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+        data-speed="800"
+        data-start="1200"
+        data-easing="easeOutQuad"
+        data-splitin="none"
+        data-splitout="none"
+        data-elementdelay="0.01"
+        data-endelementdelay="0.1"
+        data-endspeed="1000"
+        data-endeasing="Power4.easeIn" style="font-size: 56px; font-weight: 700;max-width: 630px; white-space: normal;line-height: 1.15;">Beauty with Confidence</div>
+
+        <div class="tp-caption customin ltl tp-resizeme revo-slider-desc-text tleft"
+        data-x="625"
+        data-y="310"
+        data-customin="x:140;y:0;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+        data-speed="800"
+        data-start="1400"
+        data-easing="easeOutQuad"
+        data-splitin="none"
+        data-splitout="none"
+        data-elementdelay="0.01"
+        data-endelementdelay="0.1"
+        data-endspeed="1000"
+        data-endeasing="Power4.easeIn" style="max-width: 450px; white-space: normal; font-size: 20px;">Show off your Youtube or Vimeo Videos in Style with an Autoplay Option.</div>
+
+        <div class="tp-caption customin ltl tp-resizeme"
+        data-x="625"
+        data-y="385"
+        data-customin="x:140;y:0;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+        data-speed="800"
+        data-start="1550"
+        data-easing="easeOutQuad"
+        data-splitin="none"
+        data-splitout="none"
+        data-elementdelay="0.01"
+        data-endelementdelay="0.1"
+        data-endspeed="1000"
+        data-endeasing="Power4.easeIn"><a href="#" class="button button-border button-large button-rounded tright nomargin"><span>Embed Now</span> <i class="icon-angle-right"></i></a></div>
+
+      </li>
+    </ul>
+    </div>
+    </div>
+
+    <script type="text/javascript">
+
+      jQuery(document).ready(function() {
+
+        jQuery('.tp-banner').show().revolution(
+        {
+          dottedOverlay:"none",
+          delay:16000,
+          startwidth:1140,
+          startheight:600,
+          hideThumbs:200,
+
+          thumbWidth:100,
+          thumbHeight:50,
+          thumbAmount:5,
+
+          navigationType:"bullet",
+          navigationArrows:"solo",
+          navigationStyle:"preview4",
+
+          touchenabled:"on",
+          onHoverStop:"on",
+
+          swipe_velocity: 0.7,
+          swipe_min_touches: 1,
+          swipe_max_touches: 1,
+          drag_block_vertical: false,
+
+          parallax:"mouse",
+          parallaxBgFreeze:"on",
+          parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+
+          keyboardNavigation:"off",
+
+          navigationHAlign:"center",
+          navigationVAlign:"bottom",
+          navigationHOffset:0,
+          navigationVOffset:20,
+
+          soloArrowLeftHalign:"left",
+          soloArrowLeftValign:"center",
+          soloArrowLeftHOffset:20,
+          soloArrowLeftVOffset:0,
+
+          soloArrowRightHalign:"right",
+          soloArrowRightValign:"center",
+          soloArrowRightHOffset:20,
+          soloArrowRightVOffset:0,
+
+          shadow:0,
+          fullWidth:"on",
+          fullScreen:"off",
+
+          spinner:"spinner4",
+
+          stopLoop:"off",
+          stopAfterLoops:-1,
+          stopAtSlide:-1,
+
+          shuffle:"off",
+
+          autoHeight:"off",
+          forceFullWidth:"off",
+          hideTimerBar:"on",
+
+
+          hideThumbsOnMobile:"off",
+          hideNavDelayOnMobile:1500,
+          hideBulletsOnMobile:"off",
+          hideArrowsOnMobile:"off",
+          hideThumbsUnderResolution:0,
+
+          hideSliderAtLimit:0,
+          hideCaptionAtLimit:0,
+          hideAllCaptionAtLilmit:0,
+          startWithSlide:0,
+          fullScreenOffsetContainer: ".header"
+        });
+
+      }); //ready
+
+    </script>
+
+    <!-- END REVOLUTION SLIDER -->
+
+  </section>
+
+
+	<section id="content">
+		<div class="content-wrap">
+			<div class="container clearfix">
+
+				<div class="promo promo-dark promo-flat bottommargin">
+					<h3>Post Thingyan Promotion</h3>
+					<span>All treatment now 50% off</span>
+					<a href="#" class="button button-dark button-xlarge button-rounded">Call Now</a>
+				</div>
+
+				<div class="col_half">
+					<div class="fancy-title title-double-border">
+						<h1><span>Cosmetic Treatments</span></h1>
+					</div>
+					<div class="accordion clearfix">
+						<div class="acctitle">
+								<i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Chemical Peels
+						</div>
+						<div class="acc_content clearfix">This is Accordion Content</div>
+
+						<div class="acctitle">
+							<i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>IPL
+						</div>
+						<div class="acc_content clearfix">This is Accordion Content</div>
+
+						<div class="acctitle">
+							<i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Narrow Band UVB
+						</div>
+						<div class="acc_content clearfix">This is Accordion Content</div>
+
+						<div class="acctitle">
+							<i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Radio Frequency
+						</div>
+						<div class="acc_content clearfix">This is Accordion Content</div>
+					</div>
+				</div>
+				<div class="col_half col_last">
+					<div class="fancy-title title-double-border">
+						<h1><span>Skin Treatments</span></h1>
+					</div>
+					<div class="accordion clearfix">
+						<div class="acctitle">
+								<i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Acne
+						</div>
+						<div class="acc_content clearfix">This is Accordion Content</div>
+
+						<div class="acctitle">
+							<i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Facial Redness
+						</div>
+						<div class="acc_content clearfix">This is Accordion Content</div>
+
+						<div class="acctitle">
+							<i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Vitiligo
+						</div>
+						<div class="acc_content clearfix">This is Accordion Content</div>
+
+						<div class="acctitle">
+							<i class="acc-closed icon-ok-circle"></i><i class="acc-open icon-remove-circle"></i>Warts
+						</div>
+						<div class="acc_content clearfix">This is Accordion Content</div>
+					</div>
+				</div>
+
+
+				<div class="clear"></div>
+
+
+				<div class="heading-block center bottommargin-lg">
+						<h2>Machines</h2>
+						<span>We uses international standard machines.</span>
+				</div>
+
+				<div id="oc-portfolio" class="owl-carousel portfolio-carousel">
+
+					<div class="oc-item">
+						<div class="iportfolio">
+							<div class="portfolio-image">
+								<a href="portfolio-single.html">
+									<img src="images/aqua-glo.jpg">
+								</a>
+								<div class="portfolio-overlay">
+									<a href="images/portfolio/full/1.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+									<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+								</div>
+							</div>
+							<div class="portfolio-desc">
+								<h3><a href="portfolio-single.html">Aqua Glo</a></h3>
+								<span>Abrasion-Free Liquid Facial Rejuvenation</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="oc-item">
+						<div class="iportfolio">
+							<div class="portfolio-image">
+								<a href="portfolio-single.html">
+									<img src="images/ishape.jpg">
+								</a>
+								<div class="portfolio-overlay">
+									<a href="images/portfolio/full/2.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+									<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+								</div>
+							</div>
+							<div class="portfolio-desc">
+								<h3><a href="portfolio-single.html">TRIO iShape</a></h3>
+								<span>Body Sculpting System</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="oc-item">
+						<div class="iportfolio">
+							<div class="portfolio-image">
+								<a href="#">
+									<img src="images/nannoipro.jpg">
+								</a>
+								<div class="portfolio-overlay">
+									<a href="http://vimeo.com/89396394" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
+									<a href="portfolio-single-video.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+								</div>
+							</div>
+							<div class="portfolio-desc">
+								<h3><a href="portfolio-single-video.html">Nanno iPro</a></h3>
+								<span>Intense Pulsed Light System</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="oc-item">
+						<div class="iportfolio">
+							<div class="portfolio-image">
+								<a href="portfolio-single.html">
+									<img src="images/oxyoasis.jpg" alt="Console Activity">
+								</a>
+								<div class="portfolio-overlay">
+									<a href="images/portfolio/full/5.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+									<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+								</div>
+							</div>
+							<div class="portfolio-desc">
+								<h3><a href="portfolio-single.html">OxyOasis</a></h3>
+								<span>Micro Mist Facial System</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="oc-item">
+						<div class="iportfolio">
+							<div class="portfolio-image">
+								<a href="portfolio-single-video.html">
+									<img src="images/karma.jpg">
+								</a>
+								<div class="portfolio-overlay">
+									<a href="http://www.youtube.com/watch?v=kuceVNBTJio" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
+									<a href="portfolio-single-video.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+								</div>
+							</div>
+							<div class="portfolio-desc">
+								<h3><a href="portfolio-single-video.html">Skin Karma</a></h3>
+								<span>Advanced Microcell Facial Station</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="oc-item">
+						<div class="iportfolio">
+							<div class="portfolio-image">
+								<a href="portfolio-single.html">
+									<img src="images/ifrax.jpg">
+								</a>
+								<div class="portfolio-overlay">
+									<a href="images/portfolio/full/8.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
+									<a href="portfolio-single.html" class="right-icon"><i class="icon-line-ellipsis"></i></a>
+								</div>
+							</div>
+							<div class="portfolio-desc">
+								<h3><a href="portfolio-single.html">iFrax</a></h3>
+								<span>Micro-Fractional RF</span>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<script type="text/javascript">
+
+					jQuery(document).ready(function($) {
+
+						var ocPortfolio = $("#oc-portfolio");
+
+						ocPortfolio.owlCarousel({
+							margin: 20,
+							nav: true,
+							navText: ['<i class="icon-angle-left"></i>','<i class="icon-angle-right"></i>'],
+							autoplay: false,
+							autoplayHoverPause: true,
+							dots: false,
+							responsive:{
+								0:{ items:1 },
+								600:{ items:2 },
+								1000:{ items:3 },
+								1200:{ items:4 }
+							}
+						});
+
+					});
+
+				</script>
+
+			</div>
+		</div>
+	</section>
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+</body>
+</html>
